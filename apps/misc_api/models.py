36abfd7a06bd26bccc34fdbfd19b3f7a2bc18fca
Amir Khakshour <khakshour.amir@gmail.com>
2016-12-21 15:24:11 +0330

    added model definition for misc_api app

--- apps/misc_api/models.py
+++ apps/misc_api/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from django.conf import settings
+from django.utils.translation import ugettext
+from django.utils.encoding import python_2_unicode_compatible
+from django.core.validators import MinLengthValidator
 
-# Create your models here.
+from apps.misc_api.utils import PhoneValidator
+
+
+@python_2_unicode_compatible
+class UserPurchaseHistory(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL)
+    date = models.DateTimeField(max_length=255)
+    purchase = models.CharField(max_length=150, default=None, validators=[MinLengthValidator(2)])
+    purchase_id = models.IntegerField()
+    name = models.CharField(max_length=150, default=None, validators=[MinLengthValidator(2)])
+    phone_number = models.CharField(
+        validators=[
+            PhoneValidator,
+            MinLengthValidator(10)
+        ],
+        default=None,
+        max_length=20
+    )
+    address = models.TextField()
+
+    def __str__(self):
+        return ugettext('UserPurchaseHistory<%d:%s:%s:>') % (
+            self.user.pk,
+            self.date,
+            self.purchase,
+        )
