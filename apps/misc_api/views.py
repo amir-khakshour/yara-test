beb2232f196fd6b8f2d170863208119304131021
Amir Khakshour <khakshour.amir@gmail.com>
2016-12-21 15:27:10 +0330

    added views, serializer and ... for misc_api app

--- apps/misc_api/views.py
+++ apps/misc_api/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from rest_framework import permissions, viewsets
 
-# Create your views here.
+from misc_api.models import UserPurchaseHistory
+from misc_api.utils import JokingThrottling
+from misc_api.serializers import UserPurchaseHistorySerializer
+
+
+class PurchaseItemsAPIViewSet(viewsets.ModelViewSet):
+    """
+    Misc API view Set
+    """
+    throttle_classes = (JokingThrottling,)  # Custom Throttling Class
+    queryset = UserPurchaseHistory.objects.all()
+    serializer_class = UserPurchaseHistorySerializer
+    permission_classes = (
+        permissions.DjangoModelPermissions,
+    )
--- yara/urls.py
+++ yara/urls.py
@@ -1,10 +1,27 @@
-from django.conf.urls import include, url
+from django.conf.urls import url, include
+from django.conf import settings
 from django.contrib import admin
 
+# --------------------------------------
+# Base URLS
+# --------------------------------------
 urlpatterns = [
-    # Examples:
-    # url(r'^$', 'yara.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
+
+# --------------------------------------
+# Misc API URLS
+# --------------------------------------
+urlpatterns += [
+    url(r'^%s' % settings.API_PATH, include('misc_api.urls')),
+]
+
+# --------------------------------------
+# Debug toolbar
+# --------------------------------------
+if settings.DEBUG:
+    import debug_toolbar
+    urlpatterns += [
+        url(r'^__debug__/', include(debug_toolbar.urls)),
+    ]
